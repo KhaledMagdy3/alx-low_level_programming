e50aad60ad68af726518d8703f75e122475b8997
KhaledMagdy3 <magdykhaled10@gmail.com>
2023-12-16 00:50:49 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Enter point
+ *
+ * hexadicemel
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, ch;
+
+	for (n = 0; n < 10; n++)
+	{
+	putchar('0' + n);
+	}
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+	putchar(ch);
+	}
+
+	return (0);
+}
