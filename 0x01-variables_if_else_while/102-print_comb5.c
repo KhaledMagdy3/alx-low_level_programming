5901de29ab056b1d015072e62a40f29127219940
KhaledMagdy3 <magdykhaled10@gmail.com>
2023-12-16 03:59:28 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * combination 2 num 2 digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 99; x++)
+	{
+		for (y = x + 1; y <= 99; y++)
+		{
+		putchar((x / 10) + '0');
+		putchar((x % 10) + '0');
+		putchar(' ');
+		putchar((y / 10) + '0');
+		putchar((y % 10) + '0');
+		if (x < 98 || y < 99)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
