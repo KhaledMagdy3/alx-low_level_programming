b661f082564074ecde9de3a282cbd03f2ff41407
KhaledMagdy3 <magdykhaled10@gmail.com>
2023-12-22 05:14:40 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -11,12 +11,12 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i <= 57; i++)
 	{
-	putchar('0' + i);
+	_putchar(i);
 	}
 
-	putchar('\n');
+	_putchar('\n');
 
 	return (0);
 
