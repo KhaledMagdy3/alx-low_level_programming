369ad5e7f1f683b20b41d66489f6c9aac963bdc5
KhaledMagdy3 <magdykhaled10@gmail.com>
2023-12-22 12:35:09 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,10 +12,17 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 1; i <= n; i++)
+	if (n <= 0)
 	{
-		_putchar('_');
+		_putchar('\n');
 	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
 
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
