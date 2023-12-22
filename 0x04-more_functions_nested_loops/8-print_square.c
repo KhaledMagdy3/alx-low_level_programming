6491c734f5583930578978f1f296ac81fc47c280
KhaledMagdy3 <magdykhaled10@gmail.com>
2023-12-22 13:01:53 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -19,14 +19,13 @@ void print_square(int size)
 	}
 	else
 	{
-	for (i = 1; i <= size; i++)
-	{
-		for (j = 1; j <= size; j++)
+		for (i = 1; i <= size; i++)
 		{
-			_putchar('#');
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-	}
-	_putchar('\n');
 	}
 }
