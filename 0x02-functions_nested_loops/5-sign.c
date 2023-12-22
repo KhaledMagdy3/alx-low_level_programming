6bc2523741c6602b58fc952d811eb94357225cd9
KhaledMagdy3 <magdykhaled10@gmail.com>
2023-12-22 10:33:06 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@
 
 int print_sign(int n)
 {
-	if (n >= 0)
+	if (n > 0)
 	{
 		putchar('+');
 		return (1);
@@ -21,7 +21,7 @@ int print_sign(int n)
 		putchar(0);
 		return (0);
 	}
-	else if (n <= 0)
+	else if (n < 0)
 	{
 		putchar('-');
 		return (-1);
