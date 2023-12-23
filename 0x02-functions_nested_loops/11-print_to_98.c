473465f9d89e6e672260f21822abd76ee56aa081
KhaledMagdy3 <magdykhaled10@gmail.com>
2023-12-23 04:50:50 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,31 +19,25 @@ void print_to_98(int n)
 		{
 			if (i < 98)
 			{
-				_putchar(i + '0');
-				_putchar(',');
-				_putchar(' ');
+				printf("%d, ", i);
 			}
 			else
 			{
-				_putchar(i + '0');
-				_putchar('\n');
+				printf("%d\n", i);
 			}
 		}
 	}
-	else if (n >= 98)
+	else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
 			if (i > 98)
 			{
-				_putchar(i + '0');
-				_putchar(',');
-				_putchar(' ');
+				printf("%d, ", i);
 			}
 			else
 			{
-				_putchar(i + '0');
-				_putchar('\n');
+				printf("%d\n", i);
 			}
 		}
 	}
