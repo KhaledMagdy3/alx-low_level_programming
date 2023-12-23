f2aa431fa3f50722b8bc707d3be9d6676bba3179
KhaledMagdy3 <magdykhaled10@gmail.com>
2023-12-23 04:37:16 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ *
+ * @n: integer
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i < 98)
+			{
+				_putchar(i + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(i + '0');
+				_putchar('\n');
+			}
+		}
+	}
+	else if (n >= 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i > 98)
+			{
+				_putchar(i + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(i + '0');
+				_putchar('\n');
+			}
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,6 +13,7 @@ int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
 int add(int x, int y);
+void print_to_98(int n);
 int _putchar(char c);
 
 #endif /* MAIN_H */
