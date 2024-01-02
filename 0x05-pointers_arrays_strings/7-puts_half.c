01d395f33fb9a587290f63188904c1961a9ab211
KhaledMagdy3 <magdykhaled10@gmail.com>
2024-01-02 12:00:03 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -22,14 +22,14 @@ void puts_half(char *str)
 
 	if (i % 2 == 0)
 	{
-		for (j = ((i + 2) / 2); j != '\0'; j++)
+		for (j = ((i) / 2); j <= i; j++)
 		{
 			_putchar(str[j]);
 		}
 	}
 	else
 	{
-		for (j = ((i + 1) / 2); j != '\0'; j++)
+		for (j = ((i + 1) / 2); j <= i; j++)
 		{
 			_putchar(str[j]);
 		}
