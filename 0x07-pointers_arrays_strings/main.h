4b9512f43d4978f678bc4406c2489794e7144e5a
KhaledMagdy3 <magdykhaled10@gmail.com>
2024-01-08 10:19:55 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -35,5 +35,6 @@ char *leet(char *a);
 char *rot13(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
