f91d92372763de0367c5b906e66dcbf925417cfd
KhaledMagdy3 <magdykhaled10@gmail.com>
2024-01-10 13:09:01 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _sqrt_recursion_guess - function helper
+ * _sqrt_recursion - function that returns the natural square root
+ *
+ * @n: int
+ * @i: int
+ *
+ * Return: int
+ */
+
+int _sqrt_recursion_guess(int n, int i)
+{
+	int s = i * i;
+
+	if (s == n)
+	{
+		return (i);
+	}
+	else if (s < n)
+	{
+		return (_sqrt_recursion_guess(n, i + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	return (_sqrt_recursion_guess(n, 0));
+}
