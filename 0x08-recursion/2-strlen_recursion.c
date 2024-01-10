08adc9134217112fef3aba12cb4f3a4dd2c9d028
KhaledMagdy3 <magdykhaled10@gmail.com>
2024-01-10 11:43:03 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,17 +11,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int i;
-
-	i = 1;
-
 	if (*s != '\0')
 	{
-		i++;
-		_strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 	else
 	{
-		return (i);
+		return (0);
 	}
 }
