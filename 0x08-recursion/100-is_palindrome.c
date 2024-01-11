3d7c5988d859b1d62ffe1467d20d8e659dd0bb3d
KhaledMagdy3 <magdykhaled10@gmail.com>
2024-01-11 08:44:56 -0800

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * st_lengh - function length
+ *
+ * @s: char
+ *
+ * Return: int
+ */
+
+int st_lengh(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		return (1 + st_lengh(s));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome_help - function that returns 1 if a string is a palindrome
+ *
+ * @s: char
+ * @start: int
+ * @end: int
+ *
+ * Return: int
+ */
+
+int is_palindrome_help(char *s, int start, int end)
+{
+	if (s[start] != s[end])
+	{
+		return (0);
+	}
+	if (s[start] == s[end])
+	{
+		return (is_palindrome_help(s, start + 1, end - 1));
+	}
+}
+
+/**
+ * is_palindrome - function that returns 1 if a string is a palindrome
+ *
+ * @s: char
+ *
+ * Return: int
+ */
+
+int is_palindrome(char *s)
+{
+	int l = st_lengh(s);
+
+	return (is_palindrome_help(s, 0, l - 1));
+}
