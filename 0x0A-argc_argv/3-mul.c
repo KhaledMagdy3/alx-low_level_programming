9fa5c666de47de7668462e081b694edad1c10a42
KhaledMagdy3 <magdykhaled10@gmail.com>
2024-01-14 19:54:24 +0200

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - program that prints its name
+ *
+ * @argc: int
+ * @argv: char
+ *
+ * Return: int
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, x = 1;
+
+	if (argc != 3)
+	{
+		printf("error");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		x = x * atoi(argv[i]);
+	}
+	printf("%d\n", x);
+
+	return (0);
+}
