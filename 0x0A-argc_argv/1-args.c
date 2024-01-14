23af4426d149dbf00ab0e7b0ca7e31b6502f08c7
KhaledMagdy3 <magdykhaled10@gmail.com>
2024-01-14 19:25:11 +0200

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints its name
+ *
+ * @argc: int
+ * @argv: char
+ *
+ * Return: int
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
