ae3cd677e617183d88a31368f3486a43a3ad47a9
KhaledMagdy3 <magdykhaled10@gmail.com>
2024-01-15 19:00:06 +0200

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,17 +15,17 @@ char *create_array(unsigned int size, char c)
 {
 	if (size == 0)
 	{
-		return ('\0');
+		return (NULL);
 	}
 
 	char *array = (char *)malloc(size * sizeof(char));
 
-	if (array == '\0')
+	if (array == NULL)
 	{
-		return ('\0');
+		return (NULL);
 	}
 
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < size; i++)
 	{
