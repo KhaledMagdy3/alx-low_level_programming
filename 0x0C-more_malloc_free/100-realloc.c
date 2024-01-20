2e8e35e3bfa0c8122c540fe6e37236980dc59db8
KhaledMagdy3 <magdykhaled10@gmail.com>
2024-01-20 12:15:29 +0200

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	}
 
-	ptr = malloc(new size);
+	ptr = malloc(new_size);
 	if (ptr == NULL)
 	{
 		return (NULL);
