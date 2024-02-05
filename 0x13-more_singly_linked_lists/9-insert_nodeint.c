d5a9be357726ab7e472a8bb0cef0627592ca3ede
KhaledMagdy3 <magdykhaled10@gmail.com>
2024-02-05 22:05:28 +0200

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ptr;
 	unsigned int i;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	
 	temp = malloc(sizeof(listint_t));
 	if (temp == NULL)
 	{
