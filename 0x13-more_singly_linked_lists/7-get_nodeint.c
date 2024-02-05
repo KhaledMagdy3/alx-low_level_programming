aa2e7ac68d268ac503ad8553e7ac9531f4cb456b
KhaledMagdy3 <magdykhaled10@gmail.com>
2024-02-05 21:19:31 +0200

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,6 +18,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (i = 0; i < index; i++)
 	{
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+
 		ptr = ptr->next;
 	}
 
