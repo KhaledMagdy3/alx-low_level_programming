8a4556023a76d593ef668c4dc0d4fdf9ba5d6e7e
KhaledMagdy3 <magdykhaled10@gmail.com>
2024-02-05 23:01:34 +0200

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -40,7 +40,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		current = NULL;
 		return (1);
 
-		if (current == NULL)
+		if ((current == NULL) || (current->next == NULL))
 		{
 			return (-1);
 		}
