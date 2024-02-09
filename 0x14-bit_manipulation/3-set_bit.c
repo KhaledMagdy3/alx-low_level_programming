32ec248e007002185bb2da4301e9789899a1ffd2
KhaledMagdy3 <magdykhaled10@gmail.com>
2024-02-09 14:23:42 +0200

    ./d

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * powr - function to math calc power
+ *
+ * @base: int (the base)
+ * @pw: int (the power)
+ *
+ * Return: unsigned int
+*/
+
+unsigned int powr(int base, int pw)
+{
+	unsigned int result;
+
+	if (pw == 0)
+	{
+		return (1);
+	}
+
+	else if (pw == 1)
+	{
+		return (base);
+	}
+
+	result = 1;
+	while (pw > 0)
+	{
+		result = result * base;
+		pw--;
+	}
+	return (result);
+}
+
+/**
+ * set_bit - function that sets the value of a bit to 1 at a given index
+ *
+ * @n: unsigned long int *
+ * @index: unsigned int
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 32)
+		return (-1);
+
+	*n = *n | powr(2, index);
+
+	return (1);
+}
