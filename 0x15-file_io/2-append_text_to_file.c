68f0b3fec15a044432c26d367c4b79437f8e72cf
KhaledMagdy3 <magdykhaled10@gmail.com>
2024-02-13 20:15:13 +0200

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		wrt = write(filename, text_content, sizeof(text_content));
+		wrt = write(fd, text_content, strlen(text_content));
 		if (wrt == -1)
 			return (-1);
 	}
