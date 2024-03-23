45dd84812f9103ee18b735ef45a7b2c4f6ee54e9
KhaledMagdy3 <magdykhaled10@gmail.com>
2024-03-23 10:43:04 +0200

    2-add_dnodeint.mod

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -22,9 +22,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->prev = NULL;
 	new_node->next = *head;
 
-	(*head)->prev = new_node;
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
 
 	*head = new_node;
 
-	return (head);
+	return (new_node);
 }
